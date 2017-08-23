c5ef77a22f88cb3a7eae5954778999bb4c4ecf67
wen-jing666 <975908020@qq.com>
2017-08-23 18:54:19 +0800

    8-23

---
+++ node/querystring.js
@@ -0,0 +1,16 @@
+/*const querystring = require('querystring');
+const url = require('url');
+let str='http://loacIhost:789/user?foo=bar&abc=132&abc=zxs';
+console.log(url.parse(str,true));
+
+*/
+
+/*query()
+
+如果
+
+
+*/
+
+const app = require ('app');
+                                     
